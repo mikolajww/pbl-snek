80f9edf53541a55f8e8791ba0e285d8fed04272f
mikolaj <iwa-4@sr.dmcs.pl>
2018-05-14 13:30:50 +0100

    changed structure of the app, divided the app into components, added router support for simple about page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,34 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SearchboxComponent } from './forum/searchbox/searchbox.component';
+import { PostListComponent } from './forum/post-list/post-list.component';
+import { PostComponent } from './forum/post-list/post/post.component';
+import { TagSearchComponent } from './forum/tag-search/tag-search.component';
+import { AboutComponent } from './about/about.component';
+import {RouterModule, Routes} from '@angular/router';
+import { ForumComponent } from './forum/forum.component';
 
+const routes:Routes = [
+  {path: '', component:ForumComponent},
+  {path: 'about', component:AboutComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SearchboxComponent,
+    PostListComponent,
+    PostComponent,
+    TagSearchComponent,
+    AboutComponent,
+    ForumComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/forum/tag-search/tag-search.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tag-search',
+  templateUrl: './tag-search.component.html',
+  styleUrls: ['./tag-search.component.css']
+})
+export class TagSearchComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
